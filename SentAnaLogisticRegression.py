b09612834fb2359f8e3ebbdfeaaf1619a949cd53
Jordy Medina <medinajordy@hotmail.com>
2020-09-02 12:50:31 -0500

    funcional jeje

--- SentAnaLogisticRegression.py
+++ SentAnaLogisticRegression.py
@@ -20,8 +20,8 @@ class Classifier:
         stop=stop.union(list(st[0]))
         y = df['sentimientos'].values
         tweets = df['tweets'].values
-        tweets_train, tweets_test, y_train, y_test = train_test_split(tweets, y, test_size=0.10, random_state=20)
-        self.vectorizer=CountVectorizer(ngram_range=(1, 2), token_pattern=r'\b\w+\b', min_df=8, max_df=0.8, stop_words=stop)
+        tweets_train, tweets_test, y_train, y_test = train_test_split(tweets, y, test_size=0.10, random_state=0)
+        self.vectorizer=CountVectorizer( token_pattern=r'\b\w+\b', min_df=4)  #ngram_range=(1, 2), stop_words=stop
         self.vectorizer.fit(tweets_train)
         self.X_train = self.vectorizer.transform(tweets_train)
         self.X_test = self.vectorizer.transform(tweets_test)
